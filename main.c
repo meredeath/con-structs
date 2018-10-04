5f78b081e222e3060887923a08b0210a63e956da
tfabiha <tfabiha@stuy.edu>
2018-10-03 20:56:34 -0400

    adding another test case

--- main.c
+++ main.c
@@ -5,18 +5,26 @@
 int main(){
   printf("\n---------------------------\n\n");
 
+  printf("Creating new struct pokemon bulb with level = 5 and name = 'bulbasaur'\n");
   struct pokemon bulb = generate(5, "bulbasaur");
-  printf("Newly created struct pokemon s is [level = 5, name = 'bulbasaur']\n");
   printf("Printing lvl of bulb: %d\n", bulb.level);
   printf("Printing name of bulb: %s\n", bulb.name);
-  
+
+  printf("\n\n");
+
+  printf("Creating new struct pokemon charm with level = 11 and name = 'charmander'\n");
+  struct pokemon charm = generate(11, "charmander");
+  printf("Printing lvl of charm: %d\n", charm.level);
+  printf("Printing name of charm: %s\n", charm.name);
+
+
   printf("\n---------------------------\n\n");
 
   modify(&bulb, 10, "venasaur");
   printf("Modifying bulb to [level = 10, name = 'venasaur']\n");
   printf("Printing lvl of bulb: %d\n", bulb.level);
   printf("Printing name of bulb: %s\n", bulb.name);
-  
+
   printf("\n---------------------------\n\n");
 
   printf("Printing bulb using print function\n");
